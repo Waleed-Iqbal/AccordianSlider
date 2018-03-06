0fd2f88578e566f607126d66fd8146268b3c2fb8
Waleed-Iqbal <ikhawajawaleed@gmail.com>
2018-03-07 02:15:47 +0500

    Project completed

--- AccordianSlider/AccordianSlider/scripts/main.js
+++ AccordianSlider/AccordianSlider/scripts/main.js
@@ -1,1 +1,21 @@
 ﻿
+
+
+var action = "click"; // can be hover, double-click etc
+var speed = "500"; // milliseconds
+
+
+$(function () {
+    $("li.q").on(action, function () {
+        $(this).next() // next element will be the answer
+            .slideToggle(speed) // opens the answer (slides down)
+            .siblings("li.a")
+            .slideUp(); // closes all of the other opened answers
+
+        var img = $(this).children('img');
+
+        $('img').not(img).removeClass("rotate"); // every imaage except the image of clicked element
+
+        img.toggleClass("rotate");
+    });
+});
